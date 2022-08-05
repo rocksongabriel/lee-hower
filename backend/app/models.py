771bb3c83b1d7ce4d2a37a4be17e9feb397c25c4
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-05 05:32:32 +0000

    Set up sqlalchemy model for the tasks table

---
+++ backend/app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Boolean, String, Integer, TIMESTAMP
+from sqlalchemy.sql.expression import text
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from .database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String(length=255), nullable=False, index=True)
+    decription = Column(String(length=500), nullable=True, index=True)
+    created = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    urgent = Column(Boolean, nullable=False, server_default="FALSE")
+    important = Column(Boolean, nullable=False, server_default="FALSE")
+    completed = Column(Boolean, nullable=False, server_default="FALSE")
+    time_to_spend = Column(Integer, nullable=False)
+    time_spent = Column(Integer, nullable=True)
