292a382606bf28ef843978640dd28db500f9b6cd
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-05 05:36:45 +0000

    Set up the database configurations and connections required Closes #3

---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://lee_hower:testpass1234@localhost/lee_hower_db"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 # Initialize the app
