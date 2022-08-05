2dddc640ff519a8fe9f994037264a4e3e96814d4
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-05 23:22:55 +0000

    Register the tasks router int the main app. Closes #4

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 
+from app.tasks.router import router as task_router
+
 
 models.Base.metadata.create_all(bind=engine)
 
 
 # Initialize the app
 app = FastAPI()
+
+# Register the routes
+app.include_router(task_router, prefix="/tasks", tags=["Task"])
 
 
 @app.get("/")
