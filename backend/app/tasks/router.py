dad2560e33c76d88ad079671df660b385728de89
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-06 00:00:19 +0000

    Add path operation to get individual task by uuid. CCloses #6

--- backend/app/tasks/router.py
+++ backend/app/tasks/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, status, Depends
+from pydantic import UUID4
 from app.tasks import schema as tasks_schema
 from app import models
 from app.database import get_db
@@ -38,3 +39,18 @@ def get_all_tasks(db: Session = Depends(get_db)):
     tasks = db.query(models.Task).all()
 
     return tasks
+
+
+@router.get("/{uuid}", response_model=tasks_schema.TaskRead)
+def get_post(uuid: UUID4, db: Session = Depends(get_db)):
+    """API endpoint to get an individual task by it's uuid"""
+
+    task = db.query(models.Task).filter(models.Task.id == uuid).first()
+
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with id {uuid} does not exist"
+        )
+
+    return task
