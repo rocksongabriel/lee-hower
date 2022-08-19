dd8707322df59215ccd1a56b004f41e2e0c45262
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-19 16:40:59 +0000

    Add function to get the current active user.
    - This function takes the current user object and returns the
    object only if the user is active.
    
    - This closes #21

--- backend/app/auth/oauth2.py
+++ backend/app/auth/oauth2.py
@@ -105,3 +105,17 @@ async def get_current_user(
         raise credentials_exception
 
     return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    """
+    Return a user object if the user account is active.
+    """
+
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user."
+        )
+
+    return current_user
