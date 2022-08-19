27ab994de6db8f95df1648b1fa021f0fad198b3f
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-19 07:26:23 +0000

    Add function to authenticate the user.
    - Given the username (email) and password, this function will
    authenticate the user's attempted password and username.

--- backend/app/auth/oauth2.py
+++ backend/app/auth/oauth2.py
@@ -1,13 +1,37 @@
 from datetime import datetime, timedelta
 
 from jose import jwt
+from sqlalchemy.orm import Session
+from app.users.utils import verify_password
+
+from app.users.models import User
 
 
 # CONSTANTS
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 60 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24  # 1 day
-SECRET_KEY = "90565699a84ecb855fc42f1a71a931f7e7ee730cb0d9866e8a8d0d936c333077"  # TODO put this in .env
+# TODO put this in .env
+SECRET_KEY = "90565699a84ecb855fc42f1a71a931f7e7ee730cb0d9866e8a8d0d936c333077"
+
+
+def authenticate_user(
+    db: Session, username: str, password: str
+) -> bool | User:
+    """
+    Take username and password and authenticate the user'
+    username against their password.
+    Return user from db if credentials are authentic
+    """
+    user = db.query(User).filter(User.email == username).first()
+
+    if user is None:
+        return False
+
+    if not verify_password(password, user.password):
+        return False
+
+    return user
 
 
 def create_access_token(
@@ -38,6 +62,5 @@ def refresh_access_token(data: dict[str, int | str | datetime]) -> str:
     """
 
     return create_access_token(
-        data,
-        expires_delta=timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+        data, expires_delta=timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
     )
