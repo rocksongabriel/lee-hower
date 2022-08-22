9fbfb6978d54e126865418ba10669e3a7929a101
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-22 23:43:13 +0000

    Add exception file to contain common exceptions

---
+++ backend/app/exceptions.py
@@ -0,0 +1,6 @@
+from fastapi import HTTPException, status
+
+user_not_authorized_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="User unauthorized to perform operation.",
+)
