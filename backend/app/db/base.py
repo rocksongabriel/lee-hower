1f1031285582b25cdb8f855305734c068892af41
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-31 14:21:25 +0000

    Modify the tablename created by alembic

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,6 +1,6 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 
-from app.db.base_class import Base
-from app.users.models import User
-from app.tasks.models import Task
+from app.db.base_class import Base # noqa
+from app.users.models import User # noqa
+from app.tasks.models import Task # noqa
