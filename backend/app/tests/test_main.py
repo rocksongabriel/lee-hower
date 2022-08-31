55a1fedfec3ac2e909cb171e268ea2b1c8290992
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-31 16:31:30 +0000

    Add test for the main.py file

---
+++ backend/app/tests/test_main.py
@@ -0,0 +1,6 @@
+from fastapi.testclient import TestClient
+
+def test_main(client: TestClient):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"status": "API RUNNING"}
