5c852f45741df3929214f1872b12ee6bf4bd9504
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-31 14:24:07 +0000

    Import models

--- backend/app/tasks/models.py
+++ backend/app/tasks/models.py
@@ -1,8 +1,14 @@
-from sqlalchemy import Column, Boolean, ForeignKey, String, Integer, TIMESTAMP
-from sqlalchemy.sql.expression import text
-from sqlalchemy.dialects.postgresql import UUID
+from typing import TYPE_CHECKING
 import uuid
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql.expression import text
+
 from app.db.base_class import Base
+
+if TYPE_CHECKING:
+    from app.users.models import User
 
 
 class Task(Base):
--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING
 from uuid import uuid4
+
 from sqlalchemy import Column
-from sqlalchemy.types import Boolean, String, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql.expression import text
+from sqlalchemy.types import Boolean, String, TIMESTAMP
+
 from app.db.base_class import Base
 
+
+if TYPE_CHECKING:
+    from app.tasks.models import Task
 
 class User(Base):
     """SQLAlchemy model for Users"""
