81e0c7e51d7f8483c9b103e191efb3167f20dd77
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-31 14:17:34 +0000

    Remove initial configuration of alembic
    Add new alembic configuration

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,16 +3,18 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from app.config import get_settings
+
 from alembic import context
-from app.db.base_class import Base
-from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+settings= get_settings()
+
 config.set_main_option(
-    "sqlalchemy.url",
-    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}",
+    "sqlalchemy.url", f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}:{settings.database_port}/{settings.database_name}"
 )
 
 # Interpret the config file for Python logging.
@@ -24,6 +26,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
+from app.users.models import User
+from app.tasks.models import Task
+from app.db.base import Base
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
