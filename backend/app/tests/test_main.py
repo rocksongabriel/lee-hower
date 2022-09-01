f893d1592a94698184711e1011b62b7bb7d5bd3a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-01 16:14:36 +0000

    Add initial tests

--- backend/app/tests/test_main.py
+++ backend/app/tests/test_main.py
@@ -1,6 +1,8 @@
 from fastapi.testclient import TestClient
 
+
 def test_main(client: TestClient):
-    response = client.get('/')
+    response = client.get("/")
+    print(response)
     assert response.status_code == 200
     assert response.json() == {"status": "API RUNNING"}
---
+++ backend/app/tests/users/test_users_routes.py
@@ -0,0 +1,14 @@
+import json
+
+from fastapi.testclient import TestClient
+
+
+def test_create_user(client: TestClient) -> None:
+    data = {
+        "email": "testuser1@gmail.com",
+        "password": "testpass1234"
+    }
+
+    response = client.post("/users/register", json.dumps(data))
+
+    assert response.status_code == 201
