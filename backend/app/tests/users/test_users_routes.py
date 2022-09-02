a3c4bd0ca0f1f4a69d79a3608baecea2508dbf8b
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-02 07:51:09 +0000

    Refactor the register user test

--- backend/app/tests/users/test_users_routes.py
+++ backend/app/tests/users/test_users_routes.py
@@ -1,11 +1,16 @@
-import json
-
 from fastapi.testclient import TestClient
 
+from app.users.schemas import UserRead
 
-def test_create_user(client: TestClient) -> None:
+
+def test_register_user(client: TestClient) -> None:
     data = {"email": "testuser1@gmail.com", "password": "testpass1234"}
 
-    response = client.post("/users/register", json.dumps(data))
+    res = client.post("/users/register", json=data)
 
-    assert response.status_code == 201
+    new_user = UserRead(**res.json()).dict()
+
+    assert res.status_code == 201
+    assert "id" in new_user
+    assert "password" not in new_user
+    assert data["email"] == new_user["email"]
