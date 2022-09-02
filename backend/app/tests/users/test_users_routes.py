85da948e87aaedd498fe11409fe647dc9998604e
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-02 10:25:46 +0000

    Add test for get_users and get_user path operation

--- backend/app/tests/users/test_users_routes.py
+++ backend/app/tests/users/test_users_routes.py
@@ -1,6 +1,19 @@
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+import pytest
 
 from app.users.schemas import UserRead
+
+
+@pytest.fixture()
+def create_multiple_users(client: TestClient):
+    user1_data = {"email": "user1@gmail.com", "password": "testpass1234"}
+    user2_data = {"email": "user2@gmail.com", "password": "testpass1234"}
+
+    url = "/users/register"
+
+    client.post(url, json=user1_data)
+    client.post(url, json=user2_data)
 
 
 def test_register_user(client: TestClient) -> None:
@@ -14,3 +27,26 @@ def test_register_user(client: TestClient) -> None:
     assert "id" in new_user
     assert "password" not in new_user
     assert data["email"] == new_user["email"]
+
+
+def test_get_users(
+    authClient: TestClient, create_multiple_users, app: FastAPI
+) -> None:
+    url = app.url_path_for("users:get-users")
+
+    res = authClient.get(url)
+
+    users: list[UserRead] = res.json()
+
+    assert res.status_code == 200
+    assert len(users) == 3
+
+
+def test_get_user(authClient: TestClient, app: FastAPI) -> None:
+    user_id = authClient.headers.get("user_id")
+    url = app.url_path_for("users:get-user", uuid=user_id)
+
+    authClient.headers.pop("user_id")
+    res = authClient.get(url)
+
+    assert res.status_code == 200
